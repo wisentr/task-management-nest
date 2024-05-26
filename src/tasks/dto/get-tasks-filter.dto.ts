3e0149f738c6a537aa1e82e63c7fdeac26891789
wisentr <mehmetefeekiner@gmail.com>
2024-05-26 10:39:05 +0200

    feat: filter tasks dto

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDTO {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
   search?: string;
 }
